1c720b4bae65775b55156c52a2b14a3ed4c16b42
Rafi Ramadhan <92287247+rafrmdhn@users.noreply.github.com>
2021-11-30 07:54:37 +0700

    Update gameberbasisteks.cpp

--- gameberbasisteks.cpp
+++ gameberbasisteks.cpp
@@ -72,13 +72,6 @@ int main()
     return 0;
 }
 
-/*********************************************
-    FUNCTION TO RETURN GAME STATUS
-    1 FOR GAME IS OVER WITH RESULT
-    -1 FOR GAME IS IN PROGRESS
-    O GAME IS OVER AND NO RESULT
-**********************************************/
-
 int checkwin()
 {
     if (square[1] == square[2] && square[2] == square[3])
